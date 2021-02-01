be7f814a9800076262b4703ebe2f93d772de73f1
xart22 <fajar.rn22@gmail.com>
2021-02-01 23:03:11 +0700

    adding product static

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react';
 import Navbar from "./components/header/Navbar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import {Container } from "semantic-ui-react";
+import Product from './components/product/Product';
 
 function App (){
 return(
+  <>
   <Navbar/>
+  <Container>
+    <Product/>
+  </Container>
+  </>
 )
 }
 
---
+++ src/components/product/Product.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react'
+
+
+
+const Product = () =>{
+    return(
+        <div className="content-product">
+        
+         <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+                     <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+                     <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+                     <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+                         <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+                         <Card
+              image='https://res.cloudinary.com/ruparupa-com/image/upload/f_auto,q_auto/l_ace:c5f05d:8cb580/f_auto,q_auto:eco/v1536224028/Products/121987_1.jpg'
+              description='Rp.50.000'
+              meta="Whiskas 1.2kg'"
+            />
+        </div>
+    )
+}
+
+export default Product
